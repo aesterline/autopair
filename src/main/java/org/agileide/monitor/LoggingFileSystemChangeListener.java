ddfc08b59ba946724a4311b7f2799ad967fda55f
Adam Esterline <adam@esterlines.com>
2009-07-11 08:16:00 -0600

    Added a simple LoggingFileSystemChangeListener. This listener logs file system changes using commons-logging.

---
+++ src/main/java/org/agileide/monitor/LoggingFileSystemChangeListener.java
@@ -0,0 +1,30 @@
+package org.agileide.monitor;
+
+import java.io.File;
+
+import org.apache.commons.logging.Log;
+
+public class LoggingFileSystemChangeListener implements FileSystemChangeListener
+{
+    private Log log;
+
+    public LoggingFileSystemChangeListener(Log log)
+    {
+        this.log = log;
+    }
+
+    public void newFile(File file)
+    {
+        log.info(String.format("\tnew: %s", file.getName()));
+    }
+
+    public void changedFile(File file)
+    {
+        log.info(String.format("\tchanged: %s", file.getName()));
+    }
+
+    public void deletedFile(File file)
+    {
+         log.info(String.format("\tdeleted: %s", file.getName()));
+    }
+}
