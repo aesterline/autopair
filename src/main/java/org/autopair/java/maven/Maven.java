631cdf46485c8695c4980edc89a986be2dd6508d
Adam Esterline <adam@esterlines.com>
2009-08-01 08:59:39 -0600

    Added a MavenCompilePhase

---
+++ src/main/java/org/autopair/java/maven/Maven.java
@@ -0,0 +1,10 @@
+package org.autopair.java.maven;
+
+public class Maven
+{
+    public static final String TEST_COMPILE_PHASE = "test-compile";
+
+    public void executePhase(String phaseName)
+    {
+    }
+}
