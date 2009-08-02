5dc6ef4566364d1d3500152d2a2a0aa21ff7afdf
Adam Esterline <adam@esterlines.com>
2009-08-01 19:53:13 -0600

    Added a project file factory for maven projects.

---
+++ src/main/java/org/autopair/java/IgnoredFile.java
@@ -0,0 +1,12 @@
+package org.autopair.java;
+
+public class IgnoredFile implements ProjectFile
+{
+    public void clean(Cleaner cleaner)
+    {
+    }
+
+    public void test(Tester tester)
+    {
+    }
+}
